9636dd82a315eacf2757020219dcf3260626e55a
raniyas <36111742+raniyas@users.noreply.github.com>
2020-11-13 17:33:18 +0530

    Update StringReversalIteration.java

--- StringReversalIteration.java
+++ StringReversalIteration.java
@@ -12,26 +12,26 @@ public class StringReversalIteration {
 		String result = "";
 		int i = 0;
 		
-       	Stack<Character> stack = new Stack<Character>();
-       	Stack<Character> reverse = new Stack<Character>();
-		
-        char[] c = s.toCharArray();
-		
-        for (i=0; i<c.length; i++){
-        		stack.add(c[i]);
-    	}
-        
-        for (i=0; i<c.length; i++){
-    		reverse.push(stack.pop());
-	    }
-        
-        Iterator<Character> itr = reverse.iterator();
-        while(itr.hasNext()){
-            result = result + itr.next().toString();
-        }
-        
-        System.out.println(result);
-        
+		Stack<Character> stack = new Stack<Character>();
+		Stack<Character> reverse = new Stack<Character>();
+
+		char[] c = s.toCharArray();
+
+		for (i=0; i<c.length; i++){
+				stack.add(c[i]);
+		}
+
+		for (i=0; i<c.length; i++){
+			reverse.push(stack.pop());
+		    }
+
+		Iterator<Character> itr = reverse.iterator();
+		while(itr.hasNext()){
+		    result = result + itr.next().toString();
+		}
+
+		System.out.println(result);
+
 	}
 
 }
