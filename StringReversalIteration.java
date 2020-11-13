e8042637713d777c602838995b968a660e118cdb
raniyas <36111742+raniyas@users.noreply.github.com>
2020-11-13 17:32:54 +0530

    Create StringReversalIteration.java

---
+++ StringReversalIteration.java
@@ -0,0 +1,37 @@
+//Problem --> String Reversal using Iteration / Recursion - Take sample String and print reverse of String characters.
+
+import java.util.*;
+import java.io.*;
+
+public class StringReversalIteration {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a string");
+		String s = input.readLine();
+		String result = "";
+		int i = 0;
+		
+       	Stack<Character> stack = new Stack<Character>();
+       	Stack<Character> reverse = new Stack<Character>();
+		
+        char[] c = s.toCharArray();
+		
+        for (i=0; i<c.length; i++){
+        		stack.add(c[i]);
+    	}
+        
+        for (i=0; i<c.length; i++){
+    		reverse.push(stack.pop());
+	    }
+        
+        Iterator<Character> itr = reverse.iterator();
+        while(itr.hasNext()){
+            result = result + itr.next().toString();
+        }
+        
+        System.out.println(result);
+        
+	}
+
+}
