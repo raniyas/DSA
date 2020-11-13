3797d95a7675784dc49a9c034f6fba864c5b0219
raniyas <36111742+raniyas@users.noreply.github.com>
2020-11-13 15:05:38 +0530

    Update FirstNonRepeatedCharacter.java

--- FirstNonRepeatedCharacter.java
+++ FirstNonRepeatedCharacter.java
@@ -13,27 +13,27 @@ public class FirstNonRepeatedCharacter {
 		
 		int flag, i = 0;
 		
-       	ArrayList<Character> array = new ArrayList<Character>();
-       	ArrayList<Character> result = new ArrayList<Character>();
-		
-        char[] c = s.toCharArray();
-		
-        for (i=0; i<c.length; i++){
-        		array.add(c[i]);
-    	}
-        
-        for (i=0; i<c.length-1; i++){
-        	flag = 0;
-        	for (int j=0; j<array.size(); j++){
-            	if(c[i]==array.get(j)){
-                	flag = flag+1;
-            	}
-        	}
-        	if(flag==1) {
-        		result.add(c[i]);
-        	}
-        }
-      		
-        System.out.println(result.get(0));
-	}
+		ArrayList<Character> array = new ArrayList<Character>();
+		ArrayList<Character> result = new ArrayList<Character>();
+
+		char[] c = s.toCharArray();
+
+		for (i=0; i<c.length; i++){
+				array.add(c[i]);
+		}
+
+		for (i=0; i<c.length-1; i++){
+			flag = 0;
+			for (int j=0; j<array.size(); j++){
+			if(c[i]==array.get(j)){
+				flag = flag+1;
+			}
+			}
+			if(flag==1) {
+				result.add(c[i]);
+			}
+		}
+
+		System.out.println(result.get(0));
+		}
 }
